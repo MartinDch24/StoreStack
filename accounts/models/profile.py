cee22f5a14245fc9e518f74ed228bd4c9f569900
MartinDch24 <martindoychev2007@gmail.com>
2024-11-16 01:04:34 +0200

    Alter Made the email field unique Add added User creation and User change forms

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -1,10 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-from StoreStack import settings
 
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        get_user_model(),
         on_delete=models.CASCADE,
         related_name='profile',
     )
--- accounts/models/user.py
+++ accounts/models/user.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from accounts.choices import UserType
 
 
 class StoreStackUser(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+
     user_type = models.CharField(
         max_length=10,
         choices=UserType.choices,
         default=UserType.BUYER
     )
+
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
 
     def __str__(self):
         return f"{self.username} ({self.get_user_type_display()})"

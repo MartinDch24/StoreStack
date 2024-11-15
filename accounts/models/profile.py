80075b488ead6c79768b55be0d1f0c61cab3fea8
MartinDch24 <martindoychev2007@gmail.com>
2024-11-16 00:22:59 +0200

    Add Profile model for extra fields for each account

--- accounts/models/profile.py
+++ accounts/models/profile.py
@@ -1,12 +1,20 @@
 from django.db import models
-
-from accounts.models import StoreStackUser
+from StoreStack import settings
 
 
 class Profile(models.Model):
     user = models.OneToOneField(
-        StoreStackUser,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name='profile',
     )
     profile_picture = models.ImageField(upload_to='media/profile-pictures')
     bio = models.TextField()
+
+    address_line_1 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"

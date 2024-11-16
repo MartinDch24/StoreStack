7f2033d705ab5dc7f634af9bc5225dee3c5c2f04
MartinDch24 <martindoychev2007@gmail.com>
2024-11-16 18:03:48 +0200

    Changed the admin register of the user to inherit UserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,25 +1,24 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from accounts.forms import StoreStackUserForm, StoreStackUserChangeForm
 from accounts.models import StoreStackUser
 
 
 @admin.register(StoreStackUser)
-class StoreStackUserAdmin(admin.ModelAdmin):
-        list_display = ['username', 'email', 'user_type', 'is_staff', 'is_active', 'date_joined']
-        list_filter = ['user_type', 'is_staff', 'is_active']
-        search_fields = ['username', 'email']
-        ordering = ['username']
+class StoreStackUserAdmin(UserAdmin):
+    form = StoreStackUserChangeForm
+    add_form = StoreStackUserForm
 
-        fieldsets = (
-            (None, {'fields': ('username', 'password')}),
-            ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-            ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-            ('Important dates', {'fields': ('last_login', 'date_joined')}),
-            ('User Type', {'fields': ('user_type',)}),
-        )
-        add_fieldsets = (
-            (None, {
-                'classes': ('wide',),
-                'fields': ('username', 'password1', 'password2', 'user_type'),
-            }),
-        )
+    list_display = ['username', 'email', 'user_type', 'is_staff', 'is_active']
+    list_filter = ['user_type', 'is_staff', 'is_active', 'is_superuser']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    readonly_fields = ['last_login', 'date_joined']
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('User Type', {'fields': ('user_type',)}),
+    )
